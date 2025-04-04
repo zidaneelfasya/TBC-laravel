753136f9f402f065525ff05161153c0ab0bdd8a6
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-04-05 06:08:08 +0700

    update migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('user'); // 'admin' atau 'user'
+            $table->string('profile_photo_path')->nullable(); // path foto profil
             $table->rememberToken();
             $table->timestamps();
         });
