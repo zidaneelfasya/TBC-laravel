42159b7456808cabcfb87d9c312c34b8da5995ca
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-04-11 21:04:55 +0700

    update middleware

---
+++ app/Http/Middleware/UserMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->user() && $request->user()->role === 'user') {
+            return $next($request);
+        }
+
+        return redirect('/admin/dashboard')->with('error', 'Anda harus login sebagai user biasa');
+    }
+}
