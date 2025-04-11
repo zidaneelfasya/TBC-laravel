13071d19ae7afe2d1cc97320308f624a7eb9d965
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-04-11 21:05:39 +0700

    create photo

---
+++ database/migrations/2025_04_10_145405_create_photos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('path'); 
+            $table->string('original_name'); 
+            $table->string('description')->nullable();
+            $table->integer('size'); 
+            $table->string('mime_type'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('photos');
+    }
+};
