3997c1f90ec4a00a317db6c95f76c4a7187e7092
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-04-13 23:13:20 +0700

    Update AdminLayout.tsx

--- resources/js/Pages/admin/AdminLayout.tsx
+++ resources/js/Pages/admin/AdminLayout.tsx
@@ -2,6 +2,7 @@ import { SidebarInset, SidebarProvider } from "@/Components/ui/sidebar"
 import { AppSidebar } from "@/Components/app-sidebar"
 import { SiteHeader } from "@/Components/site-header"
 import { ErrorOverlay } from "@/Components/error-overlay"
+import { Toaster } from "sonner"
 
 interface AppLayoutProps {
   children: React.ReactNode
@@ -13,6 +14,7 @@ export default function AdminLayout({ children }: AppLayoutProps) {
       <AppSidebar variant="inset" />
       <SidebarInset>
         <SiteHeader />
+        <Toaster position="top-center" />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
